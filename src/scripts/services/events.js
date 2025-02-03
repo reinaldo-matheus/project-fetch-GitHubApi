31a40c92fe3628b1dceddefca8dcb74dc1ed80e3
Matheus <reinaldomatheus011@gmail.com>
2025-02-02 21:57:44 -0300

    alterando caminha das pastas

---
+++ src/scripts/services/events.js
@@ -0,0 +1,7 @@
+import { baseUrl, repositoriesQuantity } from "../variables.js";
+
+async function getEvents(userName) {
+    const response =await fetch(`${baseUrl}/${userName}/events?per_page=${repositoriesQuantity}`)
+    return await response.json();
+    
+}
